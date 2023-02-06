b54d165322dda53e3b018e0882db22bc7e15aea9
MrRazorez <viantosa1999@gmail.com>
2023-02-06 20:11:41 +0700

    third

---
+++ readme.md
@@ -0,0 +1,6 @@
+Game Assets (Source) :
+
+Background        :  [Under The Sea](http://www.clker.com/clipart-356702.html)
+Rainbow Ball      :  [Rainbow Summer Beach Ball](https://sweetclipart.com/rainbow-summer-beach-ball-270)
+Background Music  :  Little Fish - [Quincas Moreira](https://www.youtube.com/channel/UCL1zFMJb0sthwdAlGjGbdyg/)
+Ball Sound Effect :  Cartoon Boing
--- src/render.js
+++ src/render.js
@@ -1,11 +1,16 @@
 const renderItem = document.getElementById("renderItem");
 
+const degrees = {
+    min: 0,
+    max: 360
+};
+
 const ball = {
     x: renderItem.width / 2,
     y: renderItem.height / 2,
     r: 40,
     velocity: 5,
-    angle: 40,
+    angle: Math.floor(Math.random() * ((degrees.max + 1) - degrees.min + 1) + degrees.min),
     xVelocity: 0,
     yVelocity: 0,
     checkAngle: true,
@@ -35,11 +40,26 @@ const ball = {
 
         if (ball.x <= ball.r || ball.x + ball.r >= renderItem.width) {
             ball.xVelocity = ball.xVelocity * -1;
+            ball.boing(ball.x / (renderItem.width/2));
         }
 
         if (ball.y <= ball.r || ball.y + ball.r >= renderItem.height) {
             ball.yVelocity = ball.yVelocity * -1;
+            ball.boing(ball.x / (renderItem.width/2));
         }
+    },
+    boing: (panner) => {
+        const audioCtx = new (window.AudioContext);
+        const audio = new Audio("./boing.mp3");
+        
+        const source = audioCtx.createMediaElementSource(audio);
+        const panNode = audioCtx.createStereoPanner();
+
+        panNode.pan.setValueAtTime(panner - 1, audioCtx.currentTime);
+        source.connect(panNode);
+        panNode.connect(audioCtx.destination);
+
+        audio.play();
     }
 }
 
